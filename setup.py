36ac4d12a1c1aeed94fc5b8ed54fd6a053805a43
Matt Wobensmith <mwobensmith@mozilla.com>
2018-01-31 09:47:56 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,64 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = 'fx_visual_tests'
+PACKAGE_VERSION = '0.1'
+
+INSTALL_REQUIRES = [
+    'coloredlogs',
+    'hashfs',
+    'python-dateutil',
+]
+
+TESTS_REQUIRE = [
+]
+
+DEV_REQUIRES = [
+    'nose',
+    'mock',
+    'pep8'
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
+    description='Visual Test Suite for Mozilla Firefox',
+    classifiers=[
+        'Environment :: Console',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Operating System :: Microsoft :: Windows :: Windows 7',
+        'Operating System :: Microsoft :: Windows :: Windows 8',
+        'Operating System :: Microsoft :: Windows :: Windows 8.1',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing'
+    ],
+    keywords=['mozilla', 'firefox', 'automation', 'regression-testing', 'testing'],
+    author='Matt Wobensmith',
+    author_email='mwobensmith@mozilla.com',
+    url='https://github.com/mwobensmith/fx_visual_tests',
+    download_url='https://github.com/mwobensmith/fx_visual_tests/latest.tar.gz',
+    license='MPL2',
+    packages=find_packages(),
+    include_package_data=True,  # See MANIFEST.in
+    zip_safe=False,
+    use_2to3=False,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require={'dev': DEV_REQUIRES},  # For `pip install -e .[dev]`
+    test_suite='nose.collector',
+    entry_points={
+        'console_scripts': [
+            'fx_visual_tests = fx_visual_tests.main:main'
+        ]
+    }
+)
